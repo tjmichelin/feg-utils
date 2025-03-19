1eb9e6b0b2afd6b1d2e850068ef29441b00b63dd
Thiago J Michelin <thiago.michelin@unesp.br>
2025-03-19 09:28:29 -0300

    Backup commit.

--- Linux-Tools/xubuntu-fresh-install.sh
+++ Linux-Tools/xubuntu-fresh-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Script para instalação de ferramentas comuns após instalação de um novo sistema.
+# Específico para computadores com arquitetura x86_64.
+# Desenvolvido para ser utilizado em sistemas Linux baseados em Debian.
 
 # Remove unused programs
 apt purge -y thunderbird gnome-sudoku gnome-mines sgt-puzzles sgt-launcher hexchat mousepad atril
@@ -9,7 +12,7 @@ apt update
 # Install some basic tools
 apt install -y gpg vim gedit evince wget apt-transport-https net-tools curl build-essential
 
-# Install a better theme :-)
+# Install a better looking theme :-)
 apt install -y arc-theme
 
 # Install Arduino IDE 2.x
@@ -17,12 +20,20 @@ LATEST_RELEASE_URL=https://github.com/arduino/arduino-ide/releases/latest
 FINAL_URL=$(curl -Ls -o /dev/null -w '%{url_effective}' "$LATEST_RELEASE_URL")
 VERSION=$(echo "$FINAL_URL" | awk -F'/' '{print $NF}')
 
-DOWNLOAD_URL="https://github.com/arduino/arduino-ide/releases/download/$VERSION/arduino-ide_${VERSION}_Linux_64bit.zip"
+DOWNLOAD_URL="https://github.com/arduino/arduino-ide/releases/download/${VERSION}/arduino-ide_${VERSION}_Linux_64bit.zip"
 
-cd $HOME/Downloads
+if [ -n "$SUDO_USER" ]; then
+	HOME="/Users/${SUDO_USER}"
+else
+	HOME=$HOME
+fi
+echo "Home is set to: ${HOME}"
+
+cd ${HOME}/Downloads
 wget $DOWNLOAD_URL
 mkdir arduino
 unzip arduino-ide_${VERSION}_Linux_64bit.zip -d arduino/
+rm arduino-ide_${VERSION}_Linux_64bit.zip
 
 mv arduino/ /opt/
 
@@ -33,4 +44,12 @@ RULE_FILE=/etc/udev/rules.d/99-arduino.rules
 touch $RULE_FILE
 echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2341\", GROUP=\"plugdev\", MODE=\"0666\"" >> $RULE_FILE
 
-cp $HOME/.arduino15/package_index.json $HOME/.arduino15/library_index.json
+PACKAGE_FILE=${HOME}/.arduino15/package_index.json
+
+if [ -e "${PACKAGE_FILE}" ]; then
+  cp ${PACKAGE_FILE} ${HOME}/.arduino15/library_index.json
+else
+  echo "Library json file not found."
+fi
+
+echo "Remember to reboot system for changes to take effect"
